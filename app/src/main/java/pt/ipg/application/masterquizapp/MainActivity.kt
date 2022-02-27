64382168c19abf830cfd288a39fc5c2bceda3348
Agostinho Ramos <agostinhopina095@gmail.com>
2022-02-27 18:16:57 +0000

    Start button activity

--- app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
+++ app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
@@ -1,12 +1,33 @@
 package pt.ipg.application.masterquizapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    // This function is generated when activity class is generated
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Hide status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        // This call the parent constructor
         super.onCreate(savedInstanceState)
+
+        // This is used to align the XML view to this class
         setContentView(R.layout.activity_main)
+
+        // this function to call the button
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            // Do some action
+        }
     }
 
 
