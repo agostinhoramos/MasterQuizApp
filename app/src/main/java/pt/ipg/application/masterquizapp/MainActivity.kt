c400a9b00c32e58d5181fd65e08863716203fecf
Agostinho Ramos <agostinhopina095@gmail.com>
2022-02-27 18:48:23 +0000

    Create intent activity from activity_main to question_activity

--- app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
+++ app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package pt.ipg.application.masterquizapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     // This function is generated when activity class is generated
@@ -23,10 +26,20 @@ class MainActivity : AppCompatActivity() {
         // This is used to align the XML view to this class
         setContentView(R.layout.activity_main)
 
+        // Declare input text 'name'
+        val etName = findViewById<AppCompatEditText>(R.id.et_name)
+
         // this function to call the button
         val btnStart = findViewById<Button>(R.id.btn_start)
         btnStart.setOnClickListener {
             // Do some action
+            if (etName.text.toString().isEmpty()){
+                Toast.makeText(this@MainActivity, "Please enter your name",
+                Toast.LENGTH_SHORT).show()
+            }
+            val intent = Intent(this@MainActivity, QuestionActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
