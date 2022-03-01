648599a77e55668aa63134fe19bc2c2b7a64bdb6
Agostinho Ramos <agostinhopina095@gmail.com>
2022-03-01 01:38:22 +0000

    Create Question object

---
+++ app/src/main/java/pt/ipg/application/masterquizapp/Question.kt
@@ -0,0 +1,11 @@
+package pt.ipg.application.masterquizapp
+
+data class Question (
+    val id: Int,
+    val question: String,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
