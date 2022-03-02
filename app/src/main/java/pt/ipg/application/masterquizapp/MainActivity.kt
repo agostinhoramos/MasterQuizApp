852097ea45c8932378d484c2801f69382d3f9b19
Agostinho Ramos <agostinhopina095@gmail.com>
2022-03-02 14:47:19 +0000

    Setting up Notifications

--- app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
+++ app/src/main/java/pt/ipg/application/masterquizapp/MainActivity.kt
@@ -1,6 +1,13 @@
 package pt.ipg.application.masterquizapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
@@ -8,8 +15,15 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val CHANEL_ID = "channelID"
+    val CHANEL_NAME = "channelName"
+    val NOTIFICATION_ID = 0
+
     // This function is generated when activity class is generated
     override fun onCreate(savedInstanceState: Bundle?) {
         // Hide status bar
@@ -39,6 +53,42 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             }
+        }
+
+        createNotificationChannel()
+        //setNotification()
+    }
+
+    private fun setNotification(){
+        val intent = Intent(this, ListActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        var notification = NotificationCompat.Builder(this, CHANEL_ID)
+            .setContentTitle("Top Ranking")
+            .setContentText("New ranking result")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        // Action ..
+        NotificationManagerCompat.from(this)
+            .notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotificationChannel(){
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ){
+            val channel = NotificationChannel(CHANEL_ID, CHANEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+
         }
     }
 }
